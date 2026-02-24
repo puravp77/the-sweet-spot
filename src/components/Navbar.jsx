d34d275ad502fbf10e453c4780159d25350070e6
puravp77 <patelpurav077@gmail.com>
2026-02-24 09:54:46 +0530

    Update content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
-import logo from "../assets/Logo.png";
+import logo from "../assets/favicon1.png";
 import Magnetic from "./Magnetic";
 
 function Navbar() {
